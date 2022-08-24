9bde3409ed274c49f529cd022e858917fe429a29
unknown <nominadmit@gmail.com>
2022-08-25 02:26:31 +0300

    imperfect task 4

--- task4.cs
+++ task4.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("insert the 1st number");
+string numberAstr = (Console.ReadLine());
+int a = int.Parse(numberAstr);
+
+Console.WriteLine("insert the 2nd number");
+string numberBstr = (Console.ReadLine());
+int b = int.Parse(numberBstr);
+
+Console.WriteLine("insert the 3rd number");
+string numberCstr = (Console.ReadLine());
+int c = int.Parse(numberCstr);
+
+int max = a;
+
+if (b > max);
+{
+max = b;
+}
+
+if (c > max);
+{
+max = c;
+}
+
+Console.WriteLine(max);
