55249bab6db6b63b7a34680fe2301d7723a73174
unknown <nominadmit@gmail.com>
2022-09-02 02:51:30 +0300

    task 6 complete

---
+++ task6.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (int.Parse(Console.ReadLine()) % 2 == 0)
+         Console.WriteLine("Это чётное число");
+    else Console.WriteLine("Это нечётное число");
+    Console.ReadKey();
