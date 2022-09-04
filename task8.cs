82d6f83b7df975239fa1c0d94a0fbac3712098c1
unknown <nominadmit@gmail.com>
2022-09-05 02:29:18 +0300

    task 8 incomplete

---
+++ task8.cs
@@ -0,0 +1,18 @@
+﻿internal class task8
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+
+        int b = int.Parse(Console.ReadLine());
+
+        int x = 1; // x - наш счетчик
+
+        while (x <= b)
+        {
+            if (x % 2 == 0) Console.WriteLine(x); //выводим x;
+
+            x++;
+        }
+    }
+}
