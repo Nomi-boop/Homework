76cc52bbce3714159867bd1b3a47e5c5cc55e9a2
unknown <nominadmit@gmail.com>
2022-09-15 21:55:31 +0300

    19-23 fixed

--- task19-23.cs
+++ task19-23.cs
@@ -1,16 +1,19 @@
 ﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 Console.WriteLine("Задача 19");
 Console.Write("Введите число: ");
-string? number = Console.ReadLine();
+string number = Console.ReadLine();
 
-void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
     Console.WriteLine($"Ваше число: {number} - палиндром.");
   }
   else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
 }
 
-if (number!.Length == 5){
+if (number!.Length == 5)
+{
   CheckingNumber(number);
 }
 else Console.WriteLine($"Введи правильное число");
@@ -51,19 +54,23 @@ Console.WriteLine("\n Задача 23");
 Console.Write("Введите число: ");
 int cube = Convert.ToInt32(Console.ReadLine());
 
-void Cube(int[] cube){
+void Cube(int[] cube)
+{
   int counter = 0;
   int length = cube.Length;
-  while (counter <  length){
+  while (counter <  length)
+  {
     cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
     counter++;
   }
 }
 
-void PrintArry(int[] coll){
+void PrintArry(int[] coll)
+{
   int count = coll.Length;
   int index = 0;
-  while(index < count){
+  while(index < count)
+  {
     Console.Write(coll[index]+ " ");
     index++;
   }
