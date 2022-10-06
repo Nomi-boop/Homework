bc3945252034df8be45a5714042181fb773253e5
unknown <nominadmit@gmail.com>
2022-10-06 06:13:38 +0300

    tasks 64-66

---
+++ task64-68.cs
@@ -0,0 +1,43 @@
+﻿using System;
+					
+internal class tasks_64_66_68
+{
+	public static void Main()
+	{
+		// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+		
+		void PrintInterval(int a, int b)
+		{
+			int start = a;
+			int end = b;
+			if(a > b)
+			{
+				start = b;
+				end = a;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i}, ");
+			}
+		}
+		PrintInterval(1, 8);
+		
+		Console.WriteLine();
+		
+		// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+		
+		int FindSumm(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			return (end + start)*(end - start + 1)/2; //формула суммы членов арифметической прогрессии
+		}
+		
+		Console.WriteLine(FindSumm(2, 3));
+	}
+}
